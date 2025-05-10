a3c626d66b7b09475a3657249b8739851d36eacc
aaraby1377 <aaraby200001@gmail.com>
2025-05-10 22:53:50 +0530

    minor bug

--- backend/src/main/java/com/sliit/controller/LearningPlanController.java
+++ backend/src/main/java/com/sliit/controller/LearningPlanController.java
@@ -5,7 +5,7 @@ import com.sliit.service.LearningPlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/learning-plans")
